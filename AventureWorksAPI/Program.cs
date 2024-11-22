27527c4300dd4932a9d30d83e42dda3adcd30e8d
Ola_Samir <olasamirshawwal@gmail.com>
2024-11-22 22:37:51 +0200

    Commit docker containers

--- AventureWorksAPI/Program.cs
+++ AventureWorksAPI/Program.cs
@@ -1,18 +1,33 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+// Add Swagger/OpenAPI services
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API Documentation",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // Enable Swagger middleware
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Documentation v1"));
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection(); 
+app.UseAuthorization();
+app.MapControllers();
 
 var summaries = new[]
 {
@@ -21,7 +36,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
