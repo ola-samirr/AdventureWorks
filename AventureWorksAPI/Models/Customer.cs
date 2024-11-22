92dd2a22fc0fea458c7636ba637db62622fae042
Ola_Samir <olasamirshawwal@gmail.com>
2024-11-22 06:37:20 +0200

    Commit 1.database integration with API. 2.API Models and controllers

---
+++ AventureWorksAPI/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AventureWorksAPI.Models
+{
+    public class Customer
+  
+        {
+            [Key]
+            public int CustomerId { get; set; }
+
+            [Required]
+            [MaxLength(100)]
+            public string FullName { get; set; }
+
+            [EmailAddress]
+            public string Email { get; set; }
+        }
+    
+
+}
+
---
+++ AventureWorksAPI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AventureWorksAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
